86ab470d54eca8dbe03af2c986a3884f6ecba63c
Choharim <jhl8586@naver.com>
2020-11-06 10:09:35 +0900

    Add painting

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
-const canvas = document.querySelector("#  jsCanvas");
+const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 650;
+canvas.height = 650;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
-function mouseOut(event){
-  painting = false;
-}
-
-function mouseUp(event){
-  painting = false;
-}
-
-function mouseDown(event){
+function startPainting(){
   painting = true;
+}
+
+function stopPainiting(){
+  painting = false;
 }
 
 function mouseIn(event){
   const x = event.offsetX;
   const y = event.offsetY;
+  if(painting){
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  }else{
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  }
+  
+ 
 } 
 
 if(canvas){
   canvas.addEventListener("mousemove",mouseIn);
-  canvas.addEventListener("mousedown",mouseDown);
-  canvas.addEventListener("mouseup",mouseUp);
-  canvas.addEventListener("mousemove",mouseOut);
+  canvas.addEventListener("mousedown",startPainting);
+  canvas.addEventListener("mouseup",stopPainiting);
+  canvas.addEventListener("mouseleave",stopPainiting);
 }
